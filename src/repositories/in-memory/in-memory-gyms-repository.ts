9b8de6173baeb7231736ffe12c9713fa1538436a
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-02 18:59:03 -0300

    feat: add a gym repository

---
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -0,0 +1,12 @@
+import { type Gym } from '@prisma/client'
+import { type GymsRepository } from '../gyms-repository'
+
+export class InMemoryGymsRepository implements GymsRepository {
+  public items: Gym[] = []
+
+  async findById (id: string) {
+    const gym = this.items.find(item => item.id === id)
+    if (!gym) return null
+    return gym
+  }
+}
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,9 +1,13 @@
 import { type CheckIn } from '@prisma/client'
 import { type CheckInsRepository } from '@/repositories/check-ins-repository'
+import { type GymsRepository } from '@/repositories/gyms-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 interface CheckInUseCaseRequest {
   userId: string
   gymId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -11,9 +15,15 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor (private readonly checkInsRepository: CheckInsRepository) {}
+  constructor (
+    private readonly checkInsRepository: CheckInsRepository,
+    private readonly gymsRepository: GymsRepository
+  ) {}
 
   async execute ({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+    if (!gym) throw new ResourceNotFoundError()
+
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOneDate(userId, new Date())
     if (checkInOnSameDay) throw new Error()
 
