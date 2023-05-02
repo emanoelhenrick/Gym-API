cef05664b3f70b7d2b528839b5ccfc0f9beee522
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-02 18:49:05 -0300

    refactor: create a default user object and add gym repository to make sut

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,17 +1,27 @@
 import { test, describe, expect, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
 
 const makeSut = () => {
   const checkInRepository = new InMemoryCheckInsRepository()
-  const checkInUseCase = new CheckInUseCase(checkInRepository)
+  const gymsRepository = new InMemoryGymsRepository()
+  const checkInUseCase = new CheckInUseCase(checkInRepository, gymsRepository)
   return {
     checkInRepository,
+    gymsRepository,
     checkInUseCase
   }
 }
 
 describe('Check In Use Case', () => {
+  const defaultUser = {
+    gymId: 'gym-01',
+    userId: 'user-01',
+    userLatitude: 0,
+    userLongitude: 0
+  }
+
   beforeEach(() => {
     vi.useFakeTimers()
   })
@@ -22,24 +32,15 @@ describe('Check In Use Case', () => {
 
   test('Should be able to create a new Check In', async () => {
     const { checkInUseCase } = makeSut()
-    const { checkIn } = await checkInUseCase.execute({
-      gymId: 'gym-01',
-      userId: 'user-01'
-    })
+    const { checkIn } = await checkInUseCase.execute(defaultUser)
     expect(checkIn.id).toEqual(expect.any(String))
   })
 
   test('Should not be able to check in twice in the same day', async () => {
     vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
     const { checkInUseCase } = makeSut()
-    await checkInUseCase.execute({
-      gymId: 'gym-01',
-      userId: 'user-01'
-    })
-    await expect(async () => checkInUseCase.execute({
-      gymId: 'gym-01',
-      userId: 'user-01'
-    }))
+    await checkInUseCase.execute(defaultUser)
+    await expect(async () => checkInUseCase.execute(defaultUser))
       .rejects
       .toBeInstanceOf(Error)
   })
@@ -47,16 +48,10 @@ describe('Check In Use Case', () => {
   test('Should be able to check in twice in different dates', async () => {
     vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
     const { checkInUseCase } = makeSut()
-    await checkInUseCase.execute({
-      gymId: 'gym-01',
-      userId: 'user-01'
-    })
+    await checkInUseCase.execute(defaultUser)
 
     vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
-    const { checkIn } = await checkInUseCase.execute({
-      gymId: 'gym-01',
-      userId: 'user-01'
-    })
+    const { checkIn } = await checkInUseCase.execute(defaultUser)
     expect(checkIn.id).toEqual(expect.any(String))
   })
 })
