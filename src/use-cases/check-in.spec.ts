71c0b724098fd87f259751658dcae8746dd9a578
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-02 17:15:01 -0300

    test: Should not be able to check in twice in the same day

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { test, describe, expect } from 'vitest'
+import { test, describe, expect, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
 
@@ -12,8 +12,47 @@ const makeSut = () => {
 }
 
 describe('Check In Use Case', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
   test('Should be able to create a new Check In', async () => {
     const { checkInUseCase } = makeSut()
+    const { checkIn } = await checkInUseCase.execute({
+      gymId: 'gym-01',
+      userId: 'user-01'
+    })
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  test('Should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    const { checkInUseCase } = makeSut()
+    await checkInUseCase.execute({
+      gymId: 'gym-01',
+      userId: 'user-01'
+    })
+    await expect(async () => checkInUseCase.execute({
+      gymId: 'gym-01',
+      userId: 'user-01'
+    }))
+      .rejects
+      .toBeInstanceOf(Error)
+  })
+
+  test('Should be able to check in twice in different dates', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    const { checkInUseCase } = makeSut()
+    await checkInUseCase.execute({
+      gymId: 'gym-01',
+      userId: 'user-01'
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
     const { checkIn } = await checkInUseCase.execute({
       gymId: 'gym-01',
       userId: 'user-01'
