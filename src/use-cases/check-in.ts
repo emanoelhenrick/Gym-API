0fa34569818e4bd9beb3432c68722e79e32d7806
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-03 16:35:06 -0300

    feat: Should not be able to check in on distant gym

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,6 +2,7 @@ import { type CheckIn } from '@prisma/client'
 import { type CheckInsRepository } from '@/repositories/check-ins-repository'
 import { type GymsRepository } from '@/repositories/gyms-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 
 interface CheckInUseCaseRequest {
   userId: string
@@ -20,9 +21,19 @@ export class CheckInUseCase {
     private readonly gymsRepository: GymsRepository
   ) {}
 
-  async execute ({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+  async execute ({ userId, gymId, userLatitude, userLongitude }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
     const gym = await this.gymsRepository.findById(gymId)
     if (!gym) throw new ResourceNotFoundError()
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      { latitude: gym.latitude.toNumber(), longitude: gym.longitude.toNumber() }
+    )
+
+    console.log(distance)
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) throw new Error()
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
     if (checkInOnSameDay) throw new Error()
