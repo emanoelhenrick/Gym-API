dbfcabf920d7c80f243003c4771422358dbe87b3
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-05 18:12:55 -0300

    feat: add fetch user check-ins history

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,24 @@
+import { type CheckInsRepository } from '@/repositories/check-ins-repository'
+import { type CheckIn } from '@prisma/client'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor (
+    private readonly checkInsRepository: CheckInsRepository
+  ) {}
+
+  async execute ({ userId, page }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(userId, page)
+    return {
+      checkIns
+    }
+  }
+}
