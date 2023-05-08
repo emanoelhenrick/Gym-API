6c7b64acd85ff592cc648da8a2dccded65197a62
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-08 12:44:41 -0300

    feat: add fetch nearby gyms use case

---
+++ src/use-cases/fetch-nearby-gyms.ts
@@ -0,0 +1,20 @@
+import { type Gym } from '@prisma/client'
+import { type GymsRepository } from '@/repositories/protocols/gyms-repository'
+
+interface FetchNearbyGymsUseCaseRequest {
+  userLatitude: number
+  userLongitude: number
+}
+
+interface FetchNearbyGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class FetchNearbyGymsUseCase {
+  constructor (private readonly gymsRepository: GymsRepository) {}
+
+  async execute ({ userLatitude, userLongitude }: FetchNearbyGymsUseCaseRequest): Promise<FetchNearbyGymsUseCaseResponse> {
+    const gyms = await this.gymsRepository.findManyNearby({ latitude: userLatitude, longitude: userLongitude })
+    return { gyms }
+  }
+}
