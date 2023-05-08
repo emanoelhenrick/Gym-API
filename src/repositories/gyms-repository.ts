1f038deeffca8eb865f1c71a82da2dcd47c5ae67
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-08 10:59:49 -0300

    feat: add searchMany method

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -3,4 +3,5 @@ import { type Prisma, type Gym } from '@prisma/client'
 export interface GymsRepository {
   findById: (id: string) => Promise<Gym | null>
   create: (data: Prisma.GymCreateInput) => Promise<Gym>
+  searchMany: (query: string, page: number) => Promise<Gym[]>
 }
