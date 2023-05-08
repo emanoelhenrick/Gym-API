f932b2c2275e4e0e1dfefda924f3b7fe404a2091
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-08 14:55:46 -0300

    test: Should not be able to validate a check-in after 20 minutes of its creation

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,7 +1,8 @@
-import { test, describe, expect } from 'vitest'
+import { test, describe, expect, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 const makeSut = async () => {
   const checkInRepository = new InMemoryCheckInsRepository()
@@ -14,13 +15,13 @@ const makeSut = async () => {
 }
 
 describe('Validate Check In Use Case', () => {
-  // beforeEach(() => {
-  //   vi.useFakeTimers()
-  // })
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
 
-  // afterEach(() => {
-  //   vi.useRealTimers()
-  // })
+  afterEach(() => {
+    vi.useRealTimers()
+  })
 
   test('Should be able to validate the check-in', async () => {
     const { validateCheckIn, checkInRepository } = await makeSut()
@@ -42,5 +43,22 @@ describe('Validate Check In Use Case', () => {
     await expect(async () => validateCheckIn.execute({ checkInId: 'idk' }))
       .rejects
       .toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  test('Should not be able to validate a check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+    const { validateCheckIn, checkInRepository } = await makeSut()
+
+    const createdCheckIn = await checkInRepository.create({
+      gym_id: ' gym-01',
+      user_id: 'user-01'
+    })
+
+    const twentyOneMinutesOnMs = 1000 * 60 * 21
+    vi.advanceTimersByTime(twentyOneMinutesOnMs)
+
+    await expect(async () => validateCheckIn.execute({ checkInId: createdCheckIn.id }))
+      .rejects
+      .toBeInstanceOf(LateCheckInValidationError)
   })
 })
