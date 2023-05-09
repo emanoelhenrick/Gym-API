410beba5945c67eb908bf6003ff7bfc201a04af8
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-09 11:28:31 -0300

    feat: implements findById method

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -8,6 +8,15 @@ export class PrismaUsersRepository implements UserRepository {
     return user
   }
 
+  async findById (id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id
+      }
+    })
+    return user
+  }
+
   async findByEmail (email: string) {
     const user = await prisma.user.findUnique({
       where: {
