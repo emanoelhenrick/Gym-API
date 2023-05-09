d32d011c367146a2ffbb2a0860947593fa2227d4
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-09 16:09:15 -0300

    feat: implements JWT verify middleware

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,16 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { type FastifyReply, type FastifyRequest } from 'fastify'
 
 export async function profile (req: FastifyRequest, res: FastifyReply) {
-  await req.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
+  const { user } = await getUserProfile.execute({
+    userId: req.user.sub
+  })
 
-  console.log(req.user.sub)
-
-  return await res.status(200).send()
+  return await res.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined
+    }
+  })
 }
