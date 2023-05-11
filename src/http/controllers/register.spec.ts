d18db44e63af753927fcc7a1ae9599d65b84e5c7
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-11 10:53:45 -0300

    test: Should be able to register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,7 +1,25 @@
-import { describe, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-describe('testand', () => {
-  test('kkkk', () => {
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('Should be able to register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456'
+      })
+
+    expect(response.statusCode).toEqual(201)
   })
 })
