ef0e19e3474ed6eb6ac917ee2785a383688a27a3
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-12 12:03:13 -0300

    test: Should be able to create a gym

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-auth-user'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('Should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Description',
+        phone: '999999999',
+        latitude: -8.4108186,
+        longitude: -37.0507571
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
