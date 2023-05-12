21bd08c30ea67fc1f7b97caa0c15b3a8805041d4
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-12 18:49:52 -0300

    test: Should be able to refresh a token

---
+++ src/http/controllers/users/refresh.spec.ts
@@ -0,0 +1,45 @@
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Refresh Token (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('Should be able to refresh a token', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456'
+      })
+
+    const authResponse = await request(app.server)
+      .post('/sessions')
+      .send({
+        email: 'johndoe@example.com',
+        password: '123456'
+      })
+
+    const cookies = authResponse.get('Set-Cookie')
+
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String)
+    })
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken=')
+    ])
+  })
+})
