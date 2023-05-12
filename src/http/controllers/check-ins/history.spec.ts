1499b7c1e5bc978074093ee73a74fef728a46405
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-12 13:42:59 -0300

    test: Should be able to list check-in history

---
+++ src/http/controllers/check-ins/history.spec.ts
@@ -0,0 +1,99 @@
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, test, vi } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-auth-user'
+
+describe('History Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  test('Should be able to list check-in history', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Description',
+        phone: '999999999',
+        latitude: -8.4108186,
+        longitude: -37.0507571
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'Description',
+        phone: '999999999',
+        latitude: -8.4108186,
+        longitude: -37.0507571
+      })
+
+    const responseGym01 = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        query: 'JavaScript'
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    const gym_01: { id: string } = responseGym01.body.gyms[0]
+
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    await request(app.server)
+      .post(`/gyms/${gym_01.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        gymId: gym_01.id,
+        latitude: -8.4108186,
+        longitude: -37.0507571
+      })
+
+    const responseGym02 = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        query: 'TypeScript'
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    const gym_02: { id: string } = responseGym02.body.gyms[0]
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+    await request(app.server)
+      .post(`/gyms/${gym_02.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        gymId: gym_02.id,
+        latitude: -8.4108186,
+        longitude: -37.0507571
+      })
+
+    const response = await request(app.server)
+      .get('/check-ins/history')
+      .query({
+        page: 1
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkIns).toHaveLength(2)
+  })
+})
