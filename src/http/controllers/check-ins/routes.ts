5d50f3beb2515146338580f0e3e68e90795ba72a
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-12 12:01:03 -0300

    chore: add all routes to register app

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -2,11 +2,15 @@ import { type FastifyInstance } from 'fastify'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { create } from './create'
 import { validate } from './validate'
+import { history } from './history'
+import { metrics } from './metrics'
 
 export async function checkInsRoutes (app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/gyms/:gymId/check-ins', create)
 
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metrics)
   app.patch('/check-ins/:checkInId/validate', validate)
 }
