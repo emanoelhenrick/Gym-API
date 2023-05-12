101c1f36c32e8364914006379bbeaf1fc7129a9d
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-12 12:02:55 -0300

    refactor: use create and auth function

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -1,6 +1,7 @@
 import { afterAll, beforeAll, describe, expect, test } from 'vitest'
 import request from 'supertest'
 import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-auth-user'
 
 describe('Profile (e2e)', () => {
   beforeAll(async () => {
@@ -12,22 +13,7 @@ describe('Profile (e2e)', () => {
   })
 
   test('Should be able to get user profile', async () => {
-    await request(app.server)
-      .post('/users')
-      .send({
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: '123456'
-      })
-
-    const authResponse = await request(app.server)
-      .post('/sessions')
-      .send({
-        email: 'johndoe@example.com',
-        password: '123456'
-      })
-
-    const { token }: { token: string } = authResponse.body
+    const { token } = await createAndAuthenticateUser(app)
 
     const profileResponse = await request(app.server)
       .get('/me')
