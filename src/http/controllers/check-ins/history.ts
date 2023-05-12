6bf895468c7d1aacf587db84db94865a03ae33e3
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-12 11:17:51 -0300

    feat: add fetch user check-ins history controller

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,22 @@
+import { makeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-check-ins-history-use-case'
+import { type FastifyReply, type FastifyRequest } from 'fastify'
+import z from 'zod'
+
+export async function history (req: FastifyRequest, res: FastifyReply) {
+  const { page } = validateCheckInHistory(req.query)
+  const fetchUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase()
+  const checkIns = await fetchUserCheckInsHistoryUseCase.execute({
+    userId: req.user.sub,
+    page
+  })
+  return await res.status(200).send({
+    checkIns
+  })
+
+  function validateCheckInHistory (query: any) {
+    const checkInHistorySchema = z.object({
+      page: z.coerce.number().min(1).default(1)
+    })
+    return checkInHistorySchema.parse(query)
+  }
+}
