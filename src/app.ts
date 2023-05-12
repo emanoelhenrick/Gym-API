c007f8f2be32c38cabbd7dcf4616448300e573b6
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-11 23:33:41 -0300

    chore: add new app register router to gyms controllers

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import fastify, { type FastifyRequest, type FastifyError, type FastifyReply } from 'fastify'
-import { appRoutes } from './http/routes'
+import { usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import { gymsRoutes } from './http/controllers/gyms/routes'
 
 export const app = fastify()
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET
 })
-app.register(appRoutes)
+
+app.register(usersRoutes)
+app.register(gymsRoutes)
+
 app.setErrorHandler(validationError)
 
 function validationError (error: FastifyError, _req: FastifyRequest, res: FastifyReply) {
