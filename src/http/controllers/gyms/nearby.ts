4ba4e3d43a0e71a17c501635f7014da6881fc3cb
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-11 23:50:29 -0300

    feat: add fetch nearby gyms controller

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,28 @@
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+import { type FastifyReply, type FastifyRequest } from 'fastify'
+import z from 'zod'
+
+export async function nearby (req: FastifyRequest, res: FastifyReply) {
+  const { latitude, longitude } = validateNearbyGymQueryParams(req.query)
+  const fetchNearbyGyms = makeFetchNearbyGymsUseCase()
+  const fetchNearbyGymsResponse = await fetchNearbyGyms.execute({
+    userLatitude: latitude,
+    userLongitude: longitude
+  })
+  const { gyms } = fetchNearbyGymsResponse
+  return await res.status(201).send({
+    gyms
+  })
+
+  function validateNearbyGymQueryParams (query: any) {
+    const nearbyGymsQuerySchema = z.object({
+      latitude: z.number().refine(value => {
+        return Math.abs(value) <= 90
+      }),
+      longitude: z.number().refine(value => {
+        return Math.abs(value) <= 180
+      })
+    })
+    return nearbyGymsQuerySchema.parse(query)
+  }
+}
