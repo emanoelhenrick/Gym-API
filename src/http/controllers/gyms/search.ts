d470e0eb6903485a20074fdf12e9951c86598264
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-11 23:41:18 -0300

    feat: add search gym controller

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,18 @@
+import { makeSearchGymUseCase } from '@/use-cases/factories/make-search-gym-use-case'
+import { type FastifyReply, type FastifyRequest } from 'fastify'
+import z from 'zod'
+
+export async function search (req: FastifyRequest, res: FastifyReply) {
+  const { query, page } = validateSearchGymQueryParams(req.query)
+  const searchGym = makeSearchGymUseCase()
+  const searchResponse = await searchGym.execute({ query, page })
+  return await res.status(201).send(searchResponse)
+
+  function validateSearchGymQueryParams (query: any) {
+    const searchGymQuerySchema = z.object({
+      query: z.string(),
+      page: z.coerce.number().min(1).default(1)
+    })
+    return searchGymQuerySchema.parse(query)
+  }
+}
