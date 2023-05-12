8b6a9131e9520f26b84fb1583a3f610833103751
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-11 23:12:33 -0300

    test: Should be able to get user profile

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,42 @@
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('Should be able to get user profile', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456'
+      })
+
+    const authResponse = await request(app.server)
+      .post('/sessions')
+      .send({
+        email: 'johndoe@example.com',
+        password: '123456'
+      })
+
+    const { token }: { token: string } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.user).toEqual(expect.objectContaining({
+      email: 'johndoe@example.com'
+    }))
+  })
+})
