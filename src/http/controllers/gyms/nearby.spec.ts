c0e7f191d544ac5a8d221ca177cd47180651156b
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-05-12 12:56:54 -0300

    test: Should be able to list nearby gyms

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,57 @@
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-auth-user'
+
+describe('Nearby Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('Should be able to list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Near Gym',
+        latitude: -8.2884698,
+        longitude: -35.9696649,
+        description: null,
+        phone: null
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Far Gym',
+        latitude: -7.6562084,
+        longitude: -35.2760146,
+        description: null,
+        phone: null
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -8.2884698,
+        longitude: -35.9696649
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Near Gym'
+      })
+    ])
+  })
+})
